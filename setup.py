52fd1937e53404a818b4b23651201f545bd2439d
Essi Purhonen <pessiapps@gmail.com>
2024-11-24 14:11:44 +0200

    fixed setup.py file

--- setup.py
+++ setup.py
@@ -1,17 +1,8 @@
-from setuptools import setup, find_packages # type: ignore
-
-
-# Read the requirements from requirements.txt
-def read_requirements():
-    with open("requirements.txt") as f:
-        return f.read().splitlines()
-
+from setuptools import setup, find_packages  # type: ignore
 
 setup(
     name="batching-library",
     version="0.1",
     package_dir={"": "src"},
     packages=find_packages(where="src"),
-    install_requires=read_requirements(),  # Read requirements.txt for dependencies
-    python_requires=">=3.12",  # Specify the minimum Python version
 )
